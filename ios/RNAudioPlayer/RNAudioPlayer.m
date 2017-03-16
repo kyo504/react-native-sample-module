e7d52fc22c301675fee0ff93573b7d8cc82e0fee
Hun Kyo Jung <hunkyo.jung@gmail.com>
2017-03-16 18:09:04 +0900

    Clean up codes

--- ios/RNAudioPlayer/RNAudioPlayer.m
+++ ios/RNAudioPlayer/RNAudioPlayer.m
@@ -1,4 +1,3 @@
-
 #import "RNAudioPlayer.h"
 #import <React/RCTBridge.h>
 #import <React/RCTEventDispatcher.h>
@@ -7,42 +6,30 @@
 #import <Foundation/Foundation.h>
 #import <MediaPlayer/MediaPlayer.h>
 
-@interface RNAudioPlayer() {
-}
-
-@end
-
 @implementation RNAudioPlayer
 
 @synthesize bridge = _bridge;
 
-
 RCT_EXPORT_MODULE();
 
 - (RNAudioPlayer *)init {
-    self = [super init];
-    return self;
+    return [super init];
 }
 
 #pragma mark - Pubic API
 
 RCT_EXPORT_METHOD(play:(NSString *) url) {
-    if(!([url length]>0)) return;
-    
     NSURL *soundUrl = [[NSURL alloc] initWithString:url];
     self.playerItem = [AVPlayerItem playerItemWithURL:soundUrl];
     self.player = [AVPlayer playerWithPlayerItem:self.playerItem];
     
     [self.player play];
-    
     [self.bridge.eventDispatcher sendDeviceEventWithName: @"onPlaybackStateChanged"
                                                     body: @{@"state": @"PLAYING" }];
-    soundUrl = nil;
 }
 
 RCT_EXPORT_METHOD(pause) {
     [self.player pause];
-    
     [self.bridge.eventDispatcher sendDeviceEventWithName: @"onPlaybackStateChanged"
                                                     body: @{@"state": @"PAUSED" }];
 }
@@ -57,7 +44,6 @@ RCT_EXPORT_METHOD(isPlaying:(RCTResponseSenderBlock) callback)
         events[@"playing"] = @NO;
     }
 
-    callback(events]);
+    callback(@[events]);
 }
-
 @end
